5b9fe3377b4ff71557278e4cf440142fb87a821d
STEFANY <107067814+steh-cwb@users.noreply.github.com>
2024-10-17 11:38:35 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,7 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+console.log(url);
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json()
+    console.log(dados.tempo_medio);
+    }
