63d77fcfd14f7a482c61d4fda66da566c30d2a40
STEFANY <107067814+steh-cwb@users.noreply.github.com>
2024-11-14 11:28:44 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,6 +3,7 @@ console.log(url);
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas)/1e9
     const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container__texto')
 paragrafo.innerHTML = `Você sabia que o mundo tem
@@ -10,7 +11,6 @@ ${dados.total_pessoas_mundo} de pessoas e
 que aproximadamente ${dados.total_pessoas_conectadas}
 estão conectadas em alguma rede social e passam em média
 ${dados.tempo_medio} horas conectadas.`
-console.log(paragrafo)
 const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
     }
---
+++ quantidadeUsuarios.js
@@ -0,0 +1,8 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw. githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
+    }
+    quantidadeUsuarios()
+    aula 8
