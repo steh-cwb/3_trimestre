1d6b60517fbe5433385065e128597a8426e8f0c6
STEFANY <107067814+steh-cwb@users.noreply.github.com>
2024-11-28 11:47:41 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -3,6 +3,25 @@ async function quantidadeUsuarios() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+    const nomeDasRedes = Object.keys(dados)
+const quantidadeUsuarios = Object.values(dados)
     }
     quantidadeUsuarios()
-    aula 8
+    {
+    const data = [
+        {
+        x: nomeDasRedes,
+        y: quantidadeUsuarios,
+        },
+        type: 'bar',
+        marker: {
+            color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+        }
+        
+        ]
+        const grafico = document.createElement('div')
+grafico.className = 'grafico'
+
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
+        }
